c3a70af2374d0f567bcb3808aba07e0c3f34b36d
345084128 <hai14>
2018-10-08 16:18:36 +0800

    Initial commit cse2

---
+++ app/src/main/java/mengshizi/com/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package mengshizi.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
